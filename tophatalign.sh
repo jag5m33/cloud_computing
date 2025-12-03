b2f383a251eb1c72005edc65a3d2ef73b638a0ff
Jagmeet.Sandhu <jag5m33@gmail.com>
2025-12-03 11:52:14 +0000

    established the correct conda pathway and file pathways

--- tophatalign.sh
+++ tophatalign.sh
@@ -6,3 +6,15 @@
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=4
 #SBATCH --array=1-54
+
+# Load your environment
+module load miniconda3
+source activate tophat_env
+
+
+# Path to your FASTQ files
+FASTQ_DIR=/scratch/grp/msc_appbio/containersPipelines/svj_grp/E-MTAB-7657_clean/fastq
+GENOME_INDEX=/scratch/grp/msc_appbio/containersPipelines/svj_grp/E-MTAB-7657_clean/yeastGenomeIndex_bowtie2/yeast
+GTF_FILE=/scratch/grp/msc_appbio/containersPipelines/svj_grp/E-MTAB-7657_clean/yeastGenome/Saccharomyces_cerevisiae.R64-1-1.110.NCnam
+es.gtf
+OUTPUT_DIR=/scratch/grp/msc_appbio/containersPipelines/svj_grp/E-MTAB-7657_clean/tophat_results
