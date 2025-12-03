c75f583ef17f75f101e43fceae5899e134010443
Jagmeet.Sandhu <jag5m33@gmail.com>
2025-12-03 12:17:39 +0000

    added file directories and established which fastq files used for array task

--- star/rnaseqpipeline.sh
+++ star/rnaseqpipeline.sh
@@ -8,3 +8,15 @@
 #SBATCH --mem=8G
 
 echo "Start of pipeline"
+# Directories
+baseDirectory="/scratch/grp/msc_appbio/containersPipelines/svj_grp/E-MTAB-7657/"
+fastqDir="${baseDirectory}fastq/"
+resultsDirectory="${baseDirectory}results/"
+
+# Create results directory if it doesn't exist
+mkdir -p "$resultsDirectory"
+chmod 777 "$resultsDirectory"
+
+# Pick FASTQ file for this array task
+fastqFile=$(ls ${fastqDir}*.fastq.gz | sed -n "$((SLURM_ARRAY_TASK_ID+1))p")
+sampleName=$(basename "$fastqFile" .fastq.gz)
